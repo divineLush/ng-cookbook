63dc1378381ec3b665f22c31befc1dc550e47b62
divineLush <homeoflucifer@gmail.com>
2021-05-30 19:30:49 +0700

    implementing recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Recipe from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    {
+      name: 'test',
+      description: 'desc',
+      imagePath: 'https://images.unsplash.com/photo-1622130557958-f380d617a4ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'test2',
+      description: 'desc2',
+      imagePath: 'https://images.unsplash.com/photo-1622130557958-f380d617a4ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+    {
+      name: 'test2',
+      description: 'desc2',
+      imagePath: 'https://images.unsplash.com/photo-1622130557958-f380d617a4ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+    },
+  ];
 
   constructor() { }
 
